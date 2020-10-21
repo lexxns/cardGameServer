0720f5afc00c1b50065b364707824ca09dd968cc
Jordan Aspinall <lexxns@yahoo.co.uk>
2020-10-21 21:10:49 +0100

    Store cards as map for easier reducing
    Signed-off-by: Jordan Aspinall <lexxns@yahoo.co.uk>

--- src/store.go
+++ src/store.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 
+	sid "github.com/chilts/sid"
 	godux "github.com/luisvinicius167/godux"
 )
 
@@ -14,16 +15,20 @@ func initStore() {
 }
 
 func newRoom(roomID string) {
+	card1 := Card{ID: sid.Id(), Name: "card 1", Attk: 1, Health: 1, Container: "HAND"}
+	card2 := Card{ID: sid.Id(), Name: "card 2", Attk: 2, Health: 2, Container: "HAND"}
+	card3 := Card{ID: sid.Id(), Name: "card 3", Attk: 3, Health: 3, Container: "HAND"}
+	card4 := Card{ID: sid.Id(), Name: "card 4", Attk: 4, Health: 4, Container: "FIELD"}
+	card5 := Card{ID: sid.Id(), Name: "card 5", Attk: 5, Health: 5, Container: "FIELD"}
+	card6 := Card{ID: sid.Id(), Name: "card 6", Attk: 6, Health: 6, Container: "FIELD"}
 	initState := RoomState{
-		Hand: []Card{
-			{Name: "card 1"},
-			{Name: "card 2"},
-			{Name: "card 3"},
-		},
-		Field: []Card{
-			{Name: "card 4"},
-			{Name: "card 5"},
-			{Name: "card 6"},
+		Cards: map[string]Card{
+			card1.ID: card1,
+			card2.ID: card2,
+			card3.ID: card3,
+			card4.ID: card4,
+			card5.ID: card5,
+			card6.ID: card6,
 		},
 	}
 	store.SetState(roomID, initState)
@@ -31,16 +36,37 @@ func newRoom(roomID string) {
 
 // RoomState - roomState
 type RoomState struct {
-	Hand  []Card `json:"hand"`
-	Field []Card `json:"field"`
+	Cards map[string]Card `json:"cards"`
 }
 
 // Card - card
 type Card struct {
-	ID     string `json:"id"`
-	Name   string `json:"name"`
-	Attk   string `json:"attk"`
-	Health string `json:"health"`
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Attk      int    `json:"attk"`
+	Health    int    `json:"health"`
+	Container string `json:"container"`
+}
+
+func incAttk(number int) godux.Action {
+	return godux.Action{
+		Type:  "INC_ATTK",
+		Value: number,
+	}
+}
+
+func reducer(roomID string, cardID string, action godux.Action) interface{} {
+	switch action.Type {
+	case "INC_ATTK":
+		roomState := store.GetState(roomID).(RoomState)
+		var card = roomState.Cards[cardID]
+		card.Attk += action.Value.(int)
+		roomState.Cards[cardID] = card
+		return roomState
+	default:
+		return store.GetAllState()
+	}
+
 }
 
 func roomStateMessage(roomID string) []byte {
